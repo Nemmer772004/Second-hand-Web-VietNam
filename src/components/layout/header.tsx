8ac8475481a0dc6f054c284b259783ea73184982
Nam Nguyễn <namnamnamaa8@gmail.com>
2025-09-11 20:31:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -142,7 +142,7 @@ const SiteHeader = () => {
                       </NavigationMenuContent>
                     </>
                   ) : (
-                    <Link href={item.href || '#'} passHref asChild>
+                    <Link href={item.href || '#'} legacyBehavior passHref>
                       <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                         {item.name}
                       </NavigationMenuLink>
@@ -297,7 +297,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -309,7 +309,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
@@ -318,3 +318,5 @@ ListItem.displayName = "ListItem"
 
 
 export default SiteHeader;
+
+    
